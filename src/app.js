ff740c5819bf214f4261bff0378e09dbdadcf2f9
Ting <tingalex30@gmail.com>
2018-04-28 16:13:22 +0800

    boilplate app now.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@
 // console.log(validator.isEmail("ting@email.com"));
 import React from "react";
 import ReactDOM from "react-dom";
-import IndecisionApp from "./components/IndecisionApp";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("app"));
